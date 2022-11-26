2467002b2c2a5e806907293f603acc9d44779bdf
Marcin Sacha <marcindev03@gmail.com>
2022-11-26 14:18:01 +0100

    Redirect authed users to dashboard

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,10 +1,21 @@
 import type { NextPage } from "next";
 import { Box, Heading } from "@chakra-ui/react";
 import { RegisterForm } from "../components/RegisterForm/RegisterForm";
-import { useSupabaseClient } from "@supabase/auth-helpers-react";
+import { useSession, useSupabaseClient } from "@supabase/auth-helpers-react";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 const IndexPage: NextPage = () => {
+  const router = useRouter();
+
+  const session = useSession();
   const supabase = useSupabaseClient();
+
+  useEffect(() => {
+    if (session) {
+      router.push("/user/dashboard");
+    }
+  }, [session]);
 
   return (
     <Box minH="100vh" p="5">
