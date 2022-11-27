ef51a7421cf397919aba1ad6e21152533a0cc4f5
Marcin Sacha <marcindev03@gmail.com>
2022-11-27 10:44:51 +0100

    Fetch users expenses

--- apps/web/pages/user/dashboard.tsx
+++ apps/web/pages/user/dashboard.tsx
@@ -1,9 +1,15 @@
-import type { NextPage } from "next";
+import type { GetServerSideProps, NextPage } from "next";
 import { Box, Heading, Text } from "@chakra-ui/react";
 import { useSession } from "@supabase/auth-helpers-react";
 import { DashboardCard } from "ui";
+import { createServerSupabaseClient } from "@supabase/auth-helpers-nextjs";
 
-const DashboardPage: NextPage = () => {
+type DashboardPageProps = {
+  // TODO fetch types from database
+  expenses: any;
+};
+
+const DashboardPage: NextPage<DashboardPageProps> = ({ expenses }) => {
   const session = useSession();
 
   return (
@@ -23,3 +29,21 @@ const DashboardPage: NextPage = () => {
 };
 
 export default DashboardPage;
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const supabase = createServerSupabaseClient(context);
+
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
+  const { data: expenses } = await supabase.from("expenses").select("value");
+
+  return {
+    props: {
+      initialSession: session,
+      user: session?.user,
+      expenses,
+    },
+  };
+};
